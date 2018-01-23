4e17efd5f51aa8594fe820801ae96df726919b2e
zhaochuanxing <zhaochuanxing@gmail.com>
2018-01-23 23:17:41 +0800

    downloadable font

--- app/src/main/java/com/xing/ando/androido/font/DownLoadFontActivity.java
+++ app/src/main/java/com/xing/ando/androido/font/DownLoadFontActivity.java
@@ -1,5 +1,6 @@
 package com.xing.ando.androido.font;
 
+import android.support.v4.provider.FontRequest;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
@@ -11,6 +12,11 @@ public class DownLoadFontActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_down_load_font);
-        
+        downLoadFont();
+    }
+
+    private void downLoadFont() {
+//        FontRequest fontRequest = new FontRequest("com.example.fontprovider.authority",
+//                "com.example.fontprovider","my font")
     }
 }
